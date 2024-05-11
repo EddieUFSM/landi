25ebf2bcec54fee972c23d83be3ccce51a49ddf6
EddieUFSM <eddie.maneki@gmail.com>
2024-05-11 13:16:39 -0300

    add lint html

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import pluginHTML from "eslint-plugin-html"
+
+export default [
+  {
+    rules: {
+      "prefer-const": "error",
+      semi: ['error', 'never'],
+      indent: ['error', 2]
+    }
+  },
+  pluginHTML
+]
